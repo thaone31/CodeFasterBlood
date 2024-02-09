5a993108b8541ac01b00f3935419edc55bcf7752
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-10 02:35:29 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,12 +1,11 @@
+!pip install pycocotools --quiet
+!git clone https://github.com/pytorch/vision.git
+!git checkout v0.3.0
 
-pip install pycocotools --quiet
-git clone https://github.com/pytorch/vision.git
-git checkout v0.3.0
+!cp vision/references/detection/utils.py ./
+!cp vision/references/detection/transforms.py ./
+!cp vision/references/detection/coco_eval.py ./
+!cp vision/references/detection/engine.py ./
+!cp vision/references/detection/coco_utils.py ./
 
-cp vision/references/detection/utils.py ./
-cp vision/references/detection/transforms.py ./
-cp vision/references/detection/coco_eval.py ./
-cp vision/references/detection/engine.py ./
-cp vision/references/detection/coco_utils.py ./
-
-pip install roboflow
+!pip install roboflow
